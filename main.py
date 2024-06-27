19ace6c46dec24853d0e16ca7af69bf456294bcc
ramsuthar12 <b22ee081@iitj.ac.in>
2024-06-27 14:43:00 +0530

    main file updatae

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from utils import read_videos,save_video
 from trackers import Tracker
 import cv2 # type: ignore
 from team_assigner import TeamAssigner
+from player_ball_assigner import PlayerBallAssigner
 
 def main():
     #read video
-    video_frames = read_videos('input_videos/B1606b0e6_1 (36).mp4')
+    video_frames = read_videos('input_videos/08fd33_4.mp4')
 
     #Initialising Tracker
     tracker = Tracker('models/best.pt')
-    tracks = tracker.get_object_tracker(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pkl')
+    tracks = tracker.get_object_tracker(video_frames, read_from_stub=True, stub_path='stubs/track_stubs_2.pkl')
 
     # Save the croped image of a player
     # for track_id, player in tracks['players'][0].items():
@@ -22,6 +23,8 @@ def main():
     #     cv2.imwrite(f'output_videos/cropped_image.jpg', cropped_image)
     #     break
 
+    #Interpolate ball positions 
+    tracks["ball"] = tracker.interpolate_ball_position(tracks["ball"])
 
     #Assign each player it's team
     team_assigner = TeamAssigner()
@@ -33,6 +36,16 @@ def main():
 
             tracks["players"][frame_num][player_id]['team'] = team
             tracks["players"][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+
+    #Assigning the player with the ball
+    player_assigner = PlayerBallAssigner()
+    for frame_num , player_track in enumerate(tracks["players"]):
+        ball_bbox = tracks["ball"][frame_num][1]['bbox']
+        assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+
+        if assigned_player != -1:
+            tracks["players"][frame_num][assigned_player]["has_ball"] = True
 
     # Draw object tracker
     output_video_frames = tracker.draw_anotations(video_frames, tracks)
